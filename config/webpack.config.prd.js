8295add88d151095f56944bf655db3b36694c036
Serhan Apaydın <zerhan@gmail.com>
2017-12-22 16:11:11 +0300

    do not uglify output min.js

--- config/webpack.config.prd.js
+++ config/webpack.config.prd.js
@@ -10,9 +10,6 @@ config.plugins = [
     new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    new webpack.optimize.UglifyJsPlugin({
-        compressor: { warnings: false },
-    }),
 ];
 
 module.exports = config;
